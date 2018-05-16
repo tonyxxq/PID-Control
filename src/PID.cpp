f697dd8367800332566edbca309b60c242c34450
xiexq <qiangge115143@qq.com>
2018-05-16 21:38:23 +0800

    添加README.MD

--- src/PID.cpp
+++ src/PID.cpp
@@ -3,32 +3,35 @@
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
-*/
+ * TODO: Complete the PID class.
+ */
 
-PID::PID() {}
+PID::PID() {
+}
 
-PID::~PID() {}
+PID::~PID() {
+}
 
 // 初始化PID参数
 void PID::Init(double Kp, double Ki, double Kd) {
-	this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 
-	p_error = 0.0;
-	i_error = 0.0;
-	d_error = 0.0;
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
-// 更新错误
+// 更新误差
 void PID::UpdateError(double cte) {
-	p_error = cte;
-	i_error = i_error + cte;
-	d_error = cte - d_error;
+  p_error = cte;
+  i_error = i_error + cte;
+  d_error = cte - d_error;
 }
 
+// 计算总误差
 double PID::TotalError() {
-	return Kp*p_error + Ki*i_error + Kd*d_error;
+  return Kp * p_error + Ki * i_error + Kd * d_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,15 @@
 using json = nlohmann::json;
 
 // For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
+constexpr double pi() {
+  return M_PI;
+}
+double deg2rad(double x) {
+  return x * pi() / 180;
+}
+double rad2deg(double x) {
+  return x * 180 / pi();
+}
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -21,68 +27,62 @@ std::string hasData(std::string s) {
   auto b2 = s.find_last_of("]");
   if (found_null != std::string::npos) {
     return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
+  } else if (b1 != std::string::npos && b2 != std::string::npos) {
     return s.substr(b1, b2 - b1 + 1);
   }
   return "";
 }
 
-int main()
-{
+int main() {
   uWS::Hub h;
 
   PID pid;
-  // 初始化PID参数
-  double G = 1.7; // Steering gain factor.
-  double Kp = 0.05 * G;// Proportional gain 0.05
-  double Ki = 0.0005 * G; // Integral gain
-  double Kd = 0.45 * G; // Derivative gain 0.45
+  // 初始化PID参数，这个需要调节pid方面的知识
+  double G = 1.7;  // Steering gain factor.
+  double Kp = 0.05 * G;  // Proportional gain 0.05
+  double Ki = 0.0005 * G;  // Integral gain
+  double Kd = 0.45 * G;  // Derivative gain 0.45
   pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2')
-    {
-      auto s = hasData(std::string(data).substr(0, length));
-      if (s != "") {
-        auto j = json::parse(s);
-        std::string event = j[0].get<std::string>();
-        if (event == "telemetry") {
-          // j[1] is the data JSON object
-          double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
-          double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-		  
-		  pid.UpdateError();
-		  steer_value = -pid.TotalError();
-          
-          // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-
-          json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
-          auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+      if (length && length > 2 && data[0] == '4' && data[1] == '2') {
+        auto s = hasData(std::string(data).substr(0, length));
+        if (s != "") {
+          auto j = json::parse(s);
+          std::string event = j[0].get<std::string>();
+          if (event == "telemetry") {
+            // j[1] is the data JSON object
+            double cte = std::stod(j[1]["cte"].get<std::string>());
+            double speed = std::stod(j[1]["speed"].get<std::string>());
+            double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+            double steer_value;
+            /*
+             * TODO: Calcuate steering value here, remember the steering value is
+             * [-1, 1].
+             * NOTE: Feel free to play around with the throttle and speed. Maybe use
+             * another PID controller to control the speed!
+             */
+            pid.UpdateError(cte);
+            steer_value = -pid.TotalError();
+            // DEBUG
+            std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+            json msgJson;
+            msgJson["steering_angle"] = steer_value;
+            msgJson["throttle"] = 0.12;
+            auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+            std::cout << msg << std::endl;
+            ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+          }
+        } else {
+          // Manual driving
+          std::string msg = "42[\"manual\",{}]";
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-      } else {
-        // Manual driving
-        std::string msg = "42[\"manual\",{}]";
-        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
       }
-    }
-  });
+    });
 
   // We don't need this since we're not using HTTP but if it's removed the program
   // doesn't compile :-(
@@ -109,12 +109,9 @@ int main()
   });
 
   int port = 4567;
-  if (h.listen(port))
-  {
+  if (h.listen(port)) {
     std::cout << "Listening to port " << port << std::endl;
-  }
-  else
-  {
+  } else {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
